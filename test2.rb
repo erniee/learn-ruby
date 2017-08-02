2008a69ee4973edd551bc9e08efe24c6cba588aa
Ernesto Espinosa <ernesto.espinosa@gmail.com>
2017-08-02 10:16:33 -0700

    first commit

---
+++ test2.rb
@@ -0,0 +1,4 @@
+puts 1.class
+puts 1.234.class
+puts "A String".class
+
---
+++ test3.rb
@@ -0,0 +1,16 @@
+age = 12
+
+print "Enter Greeting : "
+
+greeting = gets.chomp
+
+case greeting
+when "French", "french"
+  puts "Bonjour"
+  exit
+when "Spanish", "spanish"
+  puts "Hola"
+  exit
+else "English"
+  puts "Hello"
+end
